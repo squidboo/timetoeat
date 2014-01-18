991c3a2b5b94baf8db264ea14771e4c5f04f3835
Jeni Tennison <jeni@theodi.org>
2014-01-18 23:04:51 +0000

    enabling CORS
    this is in hope that it fixes a problem with the Silent Hill font not
    being loaded in production

--- app.js
+++ app.js
@@ -32,6 +32,12 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.get('/', routes.index);
 app.post('/new', list.new);
 app.get('/about', routes.about);
